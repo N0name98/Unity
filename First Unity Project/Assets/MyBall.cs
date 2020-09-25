b1e532d01c324499907fff898f09438a0256ba99
oohana2 <oohana2@naver.com>
2020-09-25 20:27:27 +0900

    Unity-B10

---
+++ First Unity Project/Assets/MyBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        //rigid.velocity = new Vector3(2, 4, 3);
+        //rigid.AddForce(Vector3.up * 50, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetButtonDown("Jump"))
+        // {
+        //     rigid.AddForce(Vector3.up * 50, ForceMode.Impulse);
+        // }
+        // Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        // rigid.AddForce(vec, ForceMode.Impulse);
+
+        rigid.AddTorque(Vector3.up);
+    }
+}

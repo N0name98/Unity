1e4fb0189349faebcc5ab44c387c7922719198f1
oohana2 <oohana2@naver.com>
2020-09-27 18:36:00 +0900

    Unity-B15

---
+++ 2D Unity Example/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed = 4.5f;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animate;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animate = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        if (Mathf.Abs(rigid.velocity.x) < 0.1)
+        {
+            animate.SetBool("isWalking", false);
+        }
+        else
+        {
+            animate.SetBool("isWalking", true);
+        }
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1))
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+    }
+}

07ddbf193a5e6da838cdbaa9b9ae913c0a40f6ac
oohana2 <oohana2@naver.com>
2020-09-28 21:33:16 +0900

    Unity-B16

--- 2D Unity Example/Assets/Scripts/PlayerMove.cs
+++ 2D Unity Example/Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed = 4.5f;
+    public float jumoPower = 20;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator animate;
@@ -17,6 +18,11 @@ public class PlayerMove : MonoBehaviour
     }
     void Update()
     {
+        if (Input.GetButtonDown("Jump") && !animate.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumoPower, ForceMode2D.Impulse);
+            animate.SetBool("isJumping", true);
+        }
         if (Input.GetButtonDown("Horizontal"))
         {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
@@ -45,5 +51,18 @@ public class PlayerMove : MonoBehaviour
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxSpeed * (-1))
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        //Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rigid.velocity.y < 0)
+        {
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                    animate.SetBool("isJumping", false);
+            }
+        }
+
     }
 }

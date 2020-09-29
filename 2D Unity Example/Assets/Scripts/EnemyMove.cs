9e6b5efc13e49ff2af18ea930a84fa1bf867e923
oohana2 <oohana2@naver.com>
2020-09-29 22:25:56 +0900

    Unity-B18

---
+++ 2D Unity Example/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator animate;
+    SpriteRenderer spriteRenderer;
+    public int nextMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animate = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Invoke("Think", 5);
+    }
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        Vector2 frontVec = new Vector2(rigid.position.x + (nextMove * 0.5f), rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+    }
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+
+        animate.SetInteger("WalkSpeed", nextMove);
+
+        if (nextMove != 0)
+        {
+            Turn();
+        }
+
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 3);
+    }
+}

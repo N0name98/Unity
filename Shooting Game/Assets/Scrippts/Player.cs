59852b639b64072c96f7d6279a645d81f9e7aa68
oohana2 <oohana2@naver.com>
2020-10-09 19:19:43 +0900

    Unity-B27

--- Shooting Game/Assets/Scrippts/Player.cs
+++ Shooting Game/Assets/Scrippts/Player.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float speed;
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchRight;
+    public bool isTouchLeft;
 
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxisRaw("Horizontal");
+        if ((h == 1 && isTouchRight) || (h == -1 && isTouchLeft))
+            h = 0;
 
+        float v = Input.GetAxisRaw("Vertical");
+        if ((v == 1 && isTouchTop) || (v == -1 && isTouchBottom))
+            v = 0;
+
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime; //프레임에 상관없이 같은속도를 위해 deltatime 곱하기
+
+        transform.position = curPos + nextPos;
+
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
+        {
+            animator.SetInteger("Input", (int)h);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Border")
+        {
+            switch (other.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Border")
+        {
+            switch (other.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+            }
+        }
     }
 }
